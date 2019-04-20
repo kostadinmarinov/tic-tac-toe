8dd9bdeeaf31bdc066b9186732d89f4915771c34
Kostadin Marinov <k.marinov@fio.de>
2019-04-20 15:41:20 +0300

    Updates

---
+++ precache-manifest.e5ccbca5b7c46249379629442431c27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "b7399022b28359c4035a",
+    "url": "/tic-tac-toe/static/js/main.bfd5a37d.chunk.js"
+  },
+  {
+    "revision": "d3f95724ef72cd0c19fc",
+    "url": "/tic-tac-toe/static/js/2.a9d77de0.chunk.js"
+  },
+  {
+    "revision": "b7399022b28359c4035a",
+    "url": "/tic-tac-toe/static/css/main.46e68eb4.chunk.css"
+  },
+  {
+    "revision": "6189fb5f517a9ee780e7a32236f7f345",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
